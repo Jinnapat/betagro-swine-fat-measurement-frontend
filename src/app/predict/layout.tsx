a5cce04745915928cefd49653f627b606281f992
Jinnapat Yana <jaja_yana@hotmail.com>
2023-12-01 11:11:29 +0700

    add prediction layout

---
+++ src/app/predict/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/NavBar";
+
+export default function PredictionLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Navbar authenticated={true} showPredictionModes={true} />
+      <div className="pl-20 py-5 h-full">{children}</div>
+    </>
+  );
+}
