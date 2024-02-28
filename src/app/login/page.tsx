e58384f1fb4d338b7148d7a8efe86beaacc087c1
Jinnapat Yana <genesoft.ai@gmail.com>
2024-02-28 16:20:00 +0700

    remove navbar

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -49,53 +49,50 @@ export default function LoginPage() {
       needAuthorized={false}
       needUnauthorized={true}
     >
-      <>
-        <Navbar authenticated={false} showPredictionModes={false} />
-        <LayoutWithWallpaper>
-          <div className="flex flex-col w-7/12 p-20 justify-center items-center gap-4">
-            <p className="w-full">Login to AlmostSwine</p>
+      <LayoutWithWallpaper>
+        <div className="flex flex-col w-7/12 p-20 justify-center items-center gap-4">
+          <p className="w-full">Login to AlmostSwine</p>
+          <input
+            className="w-full bg-gray-100 rounded-lg p-4 outline-none"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            type="email"
+          />
+          <div className="flex flex-row w-full bg-gray-100 rounded-lg p-3 gap-2">
             <input
-              className="w-full bg-gray-100 rounded-lg p-4 outline-none"
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email"
-              type="email"
+              className="w-full bg-gray-100 outline-none"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              type={showPassword ? "text" : "password"}
             />
-            <div className="flex flex-row w-full bg-gray-100 rounded-lg p-3 gap-2">
-              <input
-                className="w-full bg-gray-100 outline-none"
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                type={showPassword ? "text" : "password"}
-              />
-              <button onClick={() => setShowPassword(!showPassword)}>
-                <Image
-                  src={showPassword ? "/show.png" : "/hidden.png"}
-                  width={30}
-                  height={30}
-                  alt="visibility"
-                />
-              </button>
-            </div>
-
-            <Button
-              text="Login"
-              colorClass="bg-purple-700"
-              handler={login}
-              disabled={email == "" || password == "" || isProcessing}
-            />
-            <p className="text-red-600">{errorMessage}</p>
-            {isProcessing && (
+            <button onClick={() => setShowPassword(!showPassword)}>
               <Image
-                src="/loading.png"
-                width={80}
-                height={80}
-                alt="loading"
-                className="absolute z-10 animate-spin"
+                src={showPassword ? "/show.png" : "/hidden.png"}
+                width={30}
+                height={30}
+                alt="visibility"
               />
-            )}
+            </button>
           </div>
-        </LayoutWithWallpaper>
-      </>
+
+          <Button
+            text="Login"
+            colorClass="bg-purple-700"
+            handler={login}
+            disabled={email == "" || password == "" || isProcessing}
+          />
+          <p className="text-red-600">{errorMessage}</p>
+          {isProcessing && (
+            <Image
+              src="/loading.png"
+              width={80}
+              height={80}
+              alt="loading"
+              className="absolute z-10 animate-spin"
+            />
+          )}
+        </div>
+      </LayoutWithWallpaper>
     </UserAuthorizationGuard>
   );
 }
