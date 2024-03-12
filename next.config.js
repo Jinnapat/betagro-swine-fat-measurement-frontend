ffe401fb984bb4aef9af0665db58f175b3342181
Jinnapat Yana <genesoft.ai@gmail.com>
2024-03-12 20:23:12 +0700

    add image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        protocol: "http",
+        port: "50505",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
